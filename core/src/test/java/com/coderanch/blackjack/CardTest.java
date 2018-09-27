ec524000efb08a14d77c7b3901eba61311a9564c
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2018-09-27 15:01:18 -0400

    Update CardTest.java
    I realized that this test automatically runs, I have added some theories and altered some method names. I'm not sure id a new test is need for each constructor? I get that the theories are assertions, but I wasn't sure how to go about adding more than one data point if this can even be done.

--- core/src/test/java/com/coderanch/blackjack/CardTest.java
+++ core/src/test/java/com/coderanch/blackjack/CardTest.java
@@ -29,11 +29,21 @@ public final class CardTest {
   
   @Theory
   public void rank_neverReturnsNull(Card card) {
-    assertThat(card.rank(), is(notNullValue()));
+    assertThat(card.getRank(), is(notNullValue()));
   }
   
   @Theory
   public void suit_neverReturnsNull(Card card) {
-    assertThat(card.suit(), is(notNullValue()));
+    assertThat(card.getSuit(), is(notNullValue()));
+  }
+  
+  @Theory
+  public void rankValue_neverReturnsNull(Card card) {
+    assertThat(card.getRankValue(), is(notNullValue()));
+  }
+  
+  @Theory
+  public void secondaryValue_neverReturnsNull(Card card) {
+    assertThat(card.getSecondaryValue(), is(notNullValue()));
   }
 }
