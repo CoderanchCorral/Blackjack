cf4265cf3e574a0eac9871157a88768bebc39dc5
aquino-a <blackterrapin@gmail.com>
2021-03-19 08:35:54 +0900

    Update core/src/main/java/com/coderanch/blackjack/Card.java
    -rank display style
    
    Co-authored-by: Stephan van Hulst <2323961+nibsi@users.noreply.github.com>

--- core/src/main/java/com/coderanch/blackjack/Card.java
+++ core/src/main/java/com/coderanch/blackjack/Card.java
@@ -25,9 +25,19 @@ final class Card implements Comparable<Card> {
      * The rank of a card.
      */
     enum Rank {
-        ACE(11), TWO(2), THREE(3), FOUR(4), FIVE(5),
-        SIX(6), SEVEN(7), EIGHT(8), NINE(9),
-        TEN(10), JACK(10), QUEEN(10), KING(10);
+        ACE   (11),
+        TWO   ( 2),
+        THREE ( 3),
+        FOUR  ( 4),
+        FIVE  ( 5),
+        SIX   ( 6),
+        SEVEN ( 7),
+        EIGHT ( 8),
+        NINE  ( 9),
+        TEN   (10),
+        JACK  (10),
+        QUEEN (10),
+        KING  (10);
 
         private int points;
 
