57364c18a9a33dfcccd5a8e7b46bd912da4748d5
Alex <29205589+aquino-a@users.noreply.github.com>
2021-03-23 08:58:02 +0900

    add isFinished test

--- core/src/test/java/com/coderanch/blackjack/GameTest.java
+++ core/src/test/java/com/coderanch/blackjack/GameTest.java
@@ -12,11 +12,12 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 
-public class GameTest {
+public final class GameTest {
+
+    private static final int LOWEST_POSSIBLE_START_SCORE = 3;
 
     /**
      * Tests whether the players get a hand of cards.
@@ -26,19 +27,43 @@ public class GameTest {
         var game = new Game(List.of(new HumanPlayer()));
         game.run();
         game.players().forEach(p -> {
-            assertThat("Has players with hands", p.hand(), is(notNullValue()));
+            assertThat("The player must have a hand.", p.hand(), is(notNullValue()));
+            assertThat(
+                    "The player must have cards in the hand.",
+                    p.hand().bestScore(),
+                    is(greaterThan(LOWEST_POSSIBLE_START_SCORE)));
         });
     }
 
     /**
-     * Tests whether the game get's new choices from the players.
+     * Tests whether the game gets new choices from the players.
      */
     @Test
     public void nextTurn() {
-        var game = new Game(List.of(new HumanPlayer()));
+        var testPlayer = new TestPlayer();
+        var game = new Game(List.of(testPlayer));
         game.run();
         game.nextTurn();
+        assertThat(
+                "The player must have chosen more than once.",
+                ((TestPlayer) game.players().get(0)).choiceCount,
+                is(greaterThan(0))
+        );
+    }
 
+    /**
+     * Tests whether the game finishes.
+     */
+    @Test
+    public void isFinished() {
+        var testPlayer = new TestPlayer();
+        var game = new Game(List.of(testPlayer));
+        assertThat("The game must not be finished.", game.isFinished(), is(equalTo(false)));
+        game.run();
+        while (!game.players().stream().allMatch(Player::isFixed)) {
+            game.nextTurn();
+        }
+        assertThat("The game must be finished.", game.isFinished(), is(equalTo(true)));
     }
 
     /**
@@ -47,6 +72,31 @@ public class GameTest {
     @Test
     public void players() {
         var game = new Game(List.of(new HumanPlayer()));
-        assertThat("The player list isn't null", game.players(), is(notNullValue()));
+        assertThat("The player list must not be null", game.players(), is(notNullValue()));
+        assertThat("The player list must not be empty", game.players().size(), is(greaterThan(0)));
+    }
+
+    private static final class TestPlayer implements Player {
+
+        /**
+         * How many times the player chose.
+         */
+        private int choiceCount;
+
+        @Override
+        public Hand hand() {
+            return null;
+        }
+
+        @Override
+        public boolean isFixed() {
+            return false;
+        }
+
+        @Override
+        public Game.CHOICE askChoice() {
+            choiceCount++;
+            return Game.CHOICE.HIT;
+        }
     }
 }
