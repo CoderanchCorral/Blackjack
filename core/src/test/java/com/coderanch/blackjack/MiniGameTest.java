4bf23157b1d81a8e859e44094681135d8171633a
aquino-a <29205589+aquino-a@users.noreply.github.com>
2022-07-05 20:21:03 +0900

    Main application loop to build a hand of cards (#32)
    Adds a small console application that repeatedly asks the user to hit or pass.
    The application reports whether the player passed, got a Blackjack or went bust.
    
    Resolves #25: Write main application loop to build a hand of cards

---
+++ core/src/test/java/com/coderanch/blackjack/MiniGameTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2018 Coderanch.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.coderanch.blackjack;
+
+import com.coderanch.util.cli.InputUtility;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+/**
+ * Tests the {@link MiniGame} class.
+ */
+public class MiniGameTest {
+
+    /**
+     * How many times to keep hitting.
+     * Ensures the loss of the game.
+     */
+    private static final int HIT_TO_FAILURE = 20;
+
+    /**
+     * Pass in the game.
+     */
+    @Test
+    public void testPass() throws IOException {
+        var input = "pass";
+        try (
+            var stream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+            var inputUtility = new InputUtility(stream, StandardCharsets.UTF_8);
+            var output = new ByteArrayOutputStream()
+        ) {
+            var miniGame = new MiniGame(
+                inputUtility,
+                new PrintWriter(output, true, StandardCharsets.UTF_8),
+                new Random());
+            miniGame.run();
+            var result = output.toString();
+            assertThat("Must say the user passed.", result, containsString("You passed."));
+        }
+    }
+
+    /**
+     * Hit until game over.
+     */
+    @Test
+    public void testHit() throws IOException {
+        var input = IntStream.range(0, HIT_TO_FAILURE)
+            .mapToObj(i -> "hit")
+            .collect(Collectors.joining(System.lineSeparator()));
+        try (
+            var stream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+            var inputUtility = new InputUtility(stream, StandardCharsets.UTF_8);
+            var output = new ByteArrayOutputStream()
+        ) {
+            var miniGame = new MiniGame(
+                inputUtility,
+                new PrintWriter(output, true, StandardCharsets.UTF_8),
+                new Random());
+            miniGame.run();
+            var result = output.toString();
+            assertThat("Must say the user lost or won.", result, containsString("Game over."));
+        }
+    }
+}
